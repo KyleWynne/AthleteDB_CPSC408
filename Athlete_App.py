7def67c77bd64c8a853a66397c7b097696852e70
Thomas Gooding <tgooding@chapman.edu>
2023-04-30 17:12:07 -0700

    Worked on update function, Added findID

--- Athlete_App.py
+++ Athlete_App.py
@@ -111,8 +111,194 @@ def query_data(): # queries data with parameters/filters
 def delete_records(): # deletes record(s)
     pass
 
+def update_options():
+    print('''
+    1. Update a player's information
+    2. Update a Team's information
+    3. Update a game's information
+    4. Update a trophies information
+    5. Exit
+    ''')
+    return helper.get_choice([1,2,3,4,5])
+
+def find_table_tuple(num):
+    num = num
+    if num == 1:
+        id = input("Enter player ID: ")
+        query = '''
+        SELECT *
+        FROM players
+        Where PlayerID = ''' + id + ";"
+    if num == 2:
+        id = input("Enter Team ID: ")
+        query = '''
+        SELECT *
+        FROM teams
+        Where teamID = ''' + id + ";"
+    if num == 3:
+        id = input("Enter game ID: ")
+        query = '''
+        SELECT *
+        FROM games
+        Where gameID = ''' + id + ";"
+    if num == 4:
+        id = input("Enter trophy ID: ")
+        query = '''
+        SELECT *
+        FROM trophies
+        Where trophyID = ''' + id + ";"           
+    if num == 5:
+        id = input("Enter League ID: ")
+        query = '''
+        SELECT *
+        FROM leagues
+        Where leagueID = ''' + id + ";"       
+    cur_obj.execute(query)
+    return cur_obj.fetchall()
+
+def findID(a, curr_name):
+    name = curr_name
+    num = a
+    if num == 1:
+        query = '''
+        SELECT playerID
+        FROM players 
+        WHERE player_name = ''' + name + ";"
+    if num == 2:
+        query = '''
+        SELECT teamID
+        FROM teams 
+        WHERE team_name = ''' + name + ";"
+    if num == 3:
+        print("DNE")
+    if num == 4:
+        query = '''
+        SELECT trophyID
+        FROM trophies 
+        WHERE trophy_name = ''' + name + ";"
+    if num == 5:
+        query = '''
+        SELECT leagueID
+        FROM leagues 
+        WHERE league_name = ''' + name + ";"
+#add try catch block??? how to account for input errors
+    return query_returnOne(query)
+
 def update_records(): # updates record(s)
-    pass
+    opt = update_options()
+    tuple = find_table_tuple(opt)
+    id = tuple[0]
+    while True:
+        if opt == 1:
+            print("What would you like to change\n")
+            print('''
+            1. player Name
+            2. yearly salary
+            3. age in years
+            4. sport
+            5. number of trophies
+            6. Team 
+            ''')
+            num = helper.get_choice[(1,2,3,4,5,6)]
+            value = input("Enter new value: ")
+            if num == 1:
+                att = "player_name"
+            if num == 2:
+                att = "salary"
+            if num == 3:
+                att = "age"
+            if num == 4:
+                att = "sport"
+            if num == 5:
+                att = "trophies"
+            if num == 6:
+                att = "teamID"
+                value = findID(2,value)
+            query = '''
+            UPDATE TABLE players 
+            SET ''' + att + " = " + value + '''
+            WHERE playerID = ''' + id + ";"
+        if opt == 2:
+            print("What would you like to change\n")
+            print('''
+            1. Team Name
+            2. League
+            3. number of trophies
+            ''')
+            num = helper.get_choice[(1,2,3,4,5,6)]
+            value = input("Enter new value: ")
+            if num == 1:
+                att = "team_name"
+            if num == 2:
+                att = "leagueID"
+                value = findID(5,value)
+            if num == 3:
+                att = "trophies"
+            query = '''
+            UPDATE TABLE teams 
+            SET ''' + att + " = " + value + '''
+            WHERE teamID = ''' + id + ";"
+        if opt == 3:
+            print("What would you like to change\n")
+            print('''
+            1. Team1 score
+            2. Team2 score
+            3. outcome
+            ''')
+            if num == 1:
+                att = "team1_score"
+            if num == 2:
+                att = "team2_score"
+            if num == 3:
+                att = "ourcome"
+            query = '''
+            UPDATE TABLE games 
+            SET ''' + att + " = " + value + '''
+            WHERE gameID = ''' + id + ";"
+        if opt == 4:
+            print("What would you like to change\n")
+            print('''
+            1. Most recent player winner
+            2. Most recent team winner
+            ''')
+            num = helper.get_choice([1,2])
+            value = input("Enter new value: ")
+            if num == 1:
+                att = "player_winner_id"
+                value = findID(1,value)
+            if num == 2:
+                att = "team_winner_id"
+                value = findID(2,value)
+            query = '''
+            UPDATE TABLE trophies 
+            SET ''' + att + " = " + value + '''
+            WHERE trophyID = ''' + id + ";"
+        if opt == 5:
+            print("Changes Done")
+            break
+    
+    
 
 def insert_records(): # inserts record(s)
     pass
+
+def startscreen():
+    print("Welcome")
+    create_all_tables()
+    num = options()
+    while True:
+        if num == 1:
+            print("Not yet completed")
+        if num == 2:
+            print("Not yet completed")
+        if num == 3:
+            print("Not yet completed")
+        if num == 4:
+            print("You chose to update a record")
+        if num == 5:
+            print("Not yet completed")
+        if num == 6:
+            print("Not yet completed")
+        if num == 7:
+            break
+
