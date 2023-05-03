f2194b09465d88abc32036fc4c6a148ba878d99c
KyleWynne <69371846+KyleWynne@users.noreply.github.com>
2023-05-03 15:42:50 -0700

    Add files via upload

--- FrontEnd.py
+++ FrontEnd.py
@@ -6,6 +6,8 @@ except ImportError:
     import Tkinter as tk     # python 2
     import tkFont as tkfont  # python 2
 
+import Backend as BE
+
 class SampleApp(tk.Tk):
 
     def __init__(self, *args, **kwargs):
@@ -14,6 +16,8 @@ class SampleApp(tk.Tk):
         self.title_font = tkfont.Font(family='Helvetica', size=18, weight="bold", slant="italic")
         self.Subsection_font = tkfont.Font(family='Arial', size=14, weight="bold", slant="italic")
         self.Info_font = tkfont.Font(family='Arial', size=12)
+
+        self.BE = BE()
 
         # the container is where we'll stack a bunch of frames
         # on top of each other, then the one we want visible
@@ -28,7 +32,6 @@ class SampleApp(tk.Tk):
             page_name = F.__name__
             frame = F(parent=container, controller=self)
             self.frames[page_name] = frame
-
             # put all of the pages in the same location;
             # the one on the top of the stacking order
             # will be the one that is visible.
@@ -48,16 +51,16 @@ class StartPage(tk.Frame):
         tk.Frame.__init__(self, parent)
         self.controller = controller
         
-        label = tk.Label(self, text="Welcome To The Athlete DB App", font=controller.title_font)
+        label = tk.Label(self, text="Welcome To The Athlete DB App", font=controller.title_font, bg='#5a3d55')
         label.pack(side="top", fill="x", pady=10)
 
-        button1 = tk.Button(self, text="Search Page",
+        button1 = tk.Button(self, text="Search Page", 
                             command=lambda: controller.show_frame("PageOne"))
         button2 = tk.Button(self, text="Update Page",
                             command=lambda: controller.show_frame("PageTwo"))
-        button3 = tk.Button(self, text="Add Page",
+        button3 = tk.Button(self, text="Add Page", 
                             command=lambda: controller.show_frame("PageThree"))
-        button4 = tk.Button(self, text="Delete Page",
+        button4 = tk.Button(self, text="Delete Page", 
                             command=lambda: controller.show_frame("PageFour"))      
 
         button1.pack()
@@ -74,6 +77,8 @@ class PageOne(tk.Frame):
 
         self.menu_options = ('Athlete','League','Club','Sport','Game')
         self.option_var = tk.StringVar(self)
+        self.KeyValue = tk.StringVar(self)
+        self.KeyName = tk.StringVar(self)
 
         label = tk.Label(self, text="Search Through our System!", font=controller.title_font)
         label.pack(side="top", fill="x", pady=10)
@@ -96,6 +101,34 @@ class PageOne(tk.Frame):
         self.output_label = tk.Label(self, font=controller.Info_font)
         self.output_label.pack()
 
+        label3 = tk.Label(self, text="Please Enter the key of what you are searching or use the search by name bar:", font=controller.Subsection_font)
+        label3.pack()
+
+        self.EnterKey = tk.Entry(self)
+        self.EnterKey.pack()
+
+        button2 = tk.Button(self, text="Next", command=self.KeyInput)
+        button2.pack()
+
+        label4 = tk.Label(self, text="Enter player name Here:", font=controller.Info_font)
+        label4.pack()
+
+        self.EnterName = tk.Entry(self,)
+        self.EnterName.pack()
+
+        button3 = tk.Button(self, text="Next", command=self.NameInput)
+        button3.pack()
+
+        label5 = tk.Label(self, font=controller.Info_font)
+        label5.pack()
+
+    def KeyInput(self, *args):
+        controller.BE.find_table_tuple(self.EnterKey.get())
+        
+    def NameInput(self, *args):
+        self.KeyValue = controller.BE.findID(self.option_var.get(), self.EnterName.get())
+        self.label5['text'] = f'That Key # is: {self.KeyValue.get()}'
+
     def optionChanged(self, *args):
         self.output_label['text'] = f'You selected: {self.option_var.get()}'
 
@@ -116,11 +149,30 @@ class PageThree(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
         self.controller = controller
+
+        self.menu_options = ('Athlete','League','Club','Sport','Game')
+        self.option_var = tk.StringVar(self)
+
         label = tk.Label(self, text="Add To Our System!", font=controller.title_font)
         label.pack(side="top", fill="x", pady=10)
         button = tk.Button(self, text="Start Page",
                            command=lambda: controller.show_frame("StartPage"))
         button.pack()
+
+        label2 = tk.Label(self, text="What Type of data would you like to Add?", font=controller.Subsection_font)
+        label2.pack()
+
+        option_menu = tk.OptionMenu(
+            self,
+            self.option_var,
+            *self.menu_options,
+            command=self.AddOptionChanged
+        )
+        option_menu.pack()
+
+    def AddOptionChanged(self, *args):
+        print(self.option_var.get())
+
 
 class PageFour(tk.Frame):
 
